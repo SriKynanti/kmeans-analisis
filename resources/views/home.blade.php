a018fd0461cd0aa83139584eb419a3e0ea51c428
SriKynanti <kynanti.ti@gmail.com>
2023-06-20 17:46:22 +0700

    edit dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,293 +2,176 @@
 
 @section('content')
 <div class="pagetitle">
-      <h1>Dashboard</h1>
-      <nav>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="/home">Home</a></li>
-          <li class="breadcrumb-item active">Dashboard</li>
-        </ol>
-      </nav>
-    </div><!-- End Page Title -->
+  <h1>Dashboard</h1>
+  <nav>
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="/home">Home</a></li>
+      <li class="breadcrumb-item active">Dashboard</li>
+    </ol>
+  </nav>
+</div><!-- End Page Title -->
 
-    <section class="section dashboard">
+<section class="section dashboard">
+  <div class="row">
+
+    <!-- Left side columns -->
+    <div class="col-lg-12">
       <div class="row">
-
-        <!-- Left side columns -->
-        <div class="col-lg-8">
-          <div class="row">
-
-            <!-- Sales Card -->
-            <div class="col-xxl-4 col-md-6">
-              <div class="card info-card sales-card">
-
-                <div class="filter">
-                  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <li class="dropdown-header text-start">
-                      <h6>Filter</h6>
-                    </li>
-
-                    <li><a class="dropdown-item" href="#">Today</a></li>
-                    <li><a class="dropdown-item" href="#">This Month</a></li>
-                    <li><a class="dropdown-item" href="#">This Year</a></li>
-                  </ul>
-                </div>
-
-                <div class="card-body">
-                  <h5 class="card-title">Sales <span>| Today</span></h5>
-
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-cart"></i>
-                    </div>
-                    <div class="ps-3">
-                      <h6>145</h6>
-                      <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>
-
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-            </div><!-- End Sales Card -->
-
-            <!-- Revenue Card -->
-            <div class="col-xxl-4 col-md-6">
-              <div class="card info-card revenue-card">
-
-                <div class="filter">
-                  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <li class="dropdown-header text-start">
-                      <h6>Filter</h6>
-                    </li>
-
-                    <li><a class="dropdown-item" href="#">Today</a></li>
-                    <li><a class="dropdown-item" href="#">This Month</a></li>
-                    <li><a class="dropdown-item" href="#">This Year</a></li>
-                  </ul>
-                </div>
-
-                <div class="card-body">
-                  <h5 class="card-title">Revenue <span>| This Month</span></h5>
-
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-currency-dollar"></i>
-                    </div>
-                    <div class="ps-3">
-                      <h6>$3,264</h6>
-                      <span class="text-success small pt-1 fw-bold">8%</span> <span class="text-muted small pt-2 ps-1">increase</span>
-
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-            </div><!-- End Revenue Card -->
-
-            <!-- Customers Card -->
-            <div class="col-xxl-4 col-xl-12">
-
-              <div class="card info-card customers-card">
-
-                <div class="filter">
-                  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <li class="dropdown-header text-start">
-                      <h6>Filter</h6>
-                    </li>
-
-                    <li><a class="dropdown-item" href="#">Today</a></li>
-                    <li><a class="dropdown-item" href="#">This Month</a></li>
-                    <li><a class="dropdown-item" href="#">This Year</a></li>
-                  </ul>
-                </div>
-
-                <div class="card-body">
-                  <h5 class="card-title">Customers <span>| This Year</span></h5>
-
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-people"></i>
-                    </div>
-                    <div class="ps-3">
-                      <h6>1244</h6>
-                      <span class="text-danger small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">decrease</span>
-
-                    </div>
-                  </div>
-
+        <div class="col-xxl-2 col-md-12">
+          <div class="card info-card sales-card">
+            <div class="card-body">
+              <div class="d-flex align-items-center">
+                <div class="ps-3 align-text-center">
+                  <h4>SILA | SMART Integration Learning Analytics</h4>
+                  <span class="font-italic small pt-2 ps-1">This system functions to perform Educational Data Mining and generate Learning Analytics by combining data from various connected applications.</span>
+                  <div><span class="text-muted small pt-2 ps-1">Sistem ini berfungsi untuk melakukan Educational Data Mining dan menghasilkan Analisa Pembelajaran (Learning Analytics) dengan menggabungkan data dari berbagai aplikasi yang sudah terhubung.</span></div>
                 </div>
               </div>
-
-            </div><!-- End Customers Card -->
-
-            <!-- Reports -->
-            <div class="col-12">
-              <div class="card">
-
-                <div class="filter">
-                  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <li class="dropdown-header text-start">
-                      <h6>Filter</h6>
-                    </li>
-
-                    <li><a class="dropdown-item" href="#">Today</a></li>
-                    <li><a class="dropdown-item" href="#">This Month</a></li>
-                    <li><a class="dropdown-item" href="#">This Year</a></li>
-                  </ul>
-                </div>
-
-                <div class="card-body">
-                  <h5 class="card-title">Reports <span>/Today</span></h5>
-
-                  <!-- Line Chart -->
-                  <div id="reportsChart"></div>
-
-                  <script>
-                    document.addEventListener("DOMContentLoaded", () => {
-                      new ApexCharts(document.querySelector("#reportsChart"), {
-                        series: [{
-                          name: 'Sales',
-                          data: [31, 40, 28, 51, 42, 82, 56],
-                        }, {
-                          name: 'Revenue',
-                          data: [11, 32, 45, 32, 34, 52, 41]
-                        }, {
-                          name: 'Customers',
-                          data: [15, 11, 32, 18, 9, 24, 11]
-                        }],
-                        chart: {
-                          height: 350,
-                          type: 'area',
-                          toolbar: {
-                            show: false
-                          },
-                        },
-                        markers: {
-                          size: 4
-                        },
-                        colors: ['#4154f1', '#2eca6a', '#ff771d'],
-                        fill: {
-                          type: "gradient",
-                          gradient: {
-                            shadeIntensity: 1,
-                            opacityFrom: 0.3,
-                            opacityTo: 0.4,
-                            stops: [0, 90, 100]
-                          }
-                        },
-                        dataLabels: {
-                          enabled: false
-                        },
-                        stroke: {
-                          curve: 'smooth',
-                          width: 2
-                        },
-                        xaxis: {
-                          type: 'datetime',
-                          categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
-                        },
-                        tooltip: {
-                          x: {
-                            format: 'dd/MM/yy HH:mm'
-                          },
-                        }
-                      }).render();
-                    });
-                  </script>
-                  <!-- End Line Chart -->
-
-                </div>
-
-              </div>
-            </div><!-- End Reports -->
-
+            </div>
           </div>
-        </div><!-- End Left side columns -->
+        </div>
+      </div>
+    </div>
 
-        <!-- Right side columns -->
-        <div class="col-lg-4">
+    <!-- Fuzzy K-Means -->
+    <div class="col-xxl-4 col-md-6">
+      <div class="card info-card revenue-card">
 
-          <!-- Budget Report -->
-          <div class="card">
-            <div class="filter">
-              <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-              <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                <li class="dropdown-header text-start">
-                  <h6>Filter</h6>
-                </li>
+        <div class="card-body">
+          <h5 class="card-title">Metode Fuzzy K-Means <span>| Muhammad Alif Ananda</span></h5>
 
-                <li><a class="dropdown-item" href="#">Today</a></li>
-                <li><a class="dropdown-item" href="#">This Month</a></li>
-                <li><a class="dropdown-item" href="#">This Year</a></li>
-              </ul>
+          <div class="d-flex">
+            <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+            <img src="assets/img/clustering.png" alt="">
             </div>
-
-            <div class="card-body pb-0">
-              <h5 class="card-title">Budget Report <span>| This Month</span></h5>
-
-              <div id="budgetChart" style="min-height: 400px;" class="echart"></div>
-
-              <script>
-                document.addEventListener("DOMContentLoaded", () => {
-                  var budgetChart = echarts.init(document.querySelector("#budgetChart")).setOption({
-                    legend: {
-                      data: ['Allocated Budget', 'Actual Spending']
-                    },
-                    radar: {
-                      // shape: 'circle',
-                      indicator: [{
-                          name: 'Sales',
-                          max: 6500
-                        },
-                        {
-                          name: 'Administration',
-                          max: 16000
-                        },
-                        {
-                          name: 'Information Technology',
-                          max: 30000
-                        },
-                        {
-                          name: 'Customer Support',
-                          max: 38000
-                        },
-                        {
-                          name: 'Development',
-                          max: 52000
-                        },
-                        {
-                          name: 'Marketing',
-                          max: 25000
-                        }
-                      ]
-                    },
-                    series: [{
-                      name: 'Budget vs spending',
-                      type: 'radar',
-                      data: [{
-                          value: [4200, 3000, 20000, 35000, 50000, 18000],
-                          name: 'Allocated Budget'
-                        },
-                        {
-                          value: [5000, 14000, 28000, 26000, 42000, 21000],
-                          name: 'Actual Spending'
-                        }
-                      ]
-                    }]
-                  });
-                });
-              </script>
-
+            <div class="ps-3">
+              <h4>Fuzzy K-Means</h4>
+              <span class="font-italic small pt-2 ps-1">Fuzzy k-means is a variation of the traditional k-means clustering algorithm that allows for a degree of uncertainty or "fuzziness" in the assignment of data points to clusters. Each iteration in FKM clustering also starts with calculating the distance of each data sample for each centroid (Hot, E., & Popović-Bugarin, V. (2016))</span>
+              <div><span class="text-muted small pt-2 ps-1">Fuzzy k-means adalah variasi dari algoritma pengelompokan k-means tradisional yang memungkinkan tingkat ketidakpastian atau "kekaburan" dalam penugasan poin data ke cluster. Setiap iterasi dalam FKM clustering juga diawali dengan menghitung jarak setiap sampel data untuk setiap centroid (Hot, E., & Popović-Bugarin, V. (2016))</span></div>
             </div>
-          </div><!-- End Budget Report -->
-
-        </div><!-- End Right side columns -->
+          </div>
+        </div>
 
       </div>
-    </section>
+    </div><!-- End Fuzzy K-Means -->
+    
+    <!-- K Means -->
+    <div class="col-xxl-4 col-md-6">
+      <div class="card info-card sales-card">
+
+        <div class="card-body">
+          <h5 class="card-title">Metode K-Means <span>| Sri Kynanti</span></h5>
+
+          <div class="d-flex">
+            <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+            <img src="assets/img/clustering.png" alt="">
+            </div>
+            <div class="ps-3">
+              <h4>K-Means</h4>
+              <span class="font-italic small pt-2 ps-1">K-means is an unsupervised grouping method that groups data into coherent groups (Abdallah Moubayed, 2020).The K-Means method divides the data into several groups (clusters) where later these data groups have the same characteristics into the same cluster and different data groups will be grouped into other clusters (Indah Purnama Sari, 2021).</span>
+              <div><span class="text-muted small pt-2 ps-1">K-means merupakan salah satu metode pengelompokan unsupervised yang melakukan pengelompokan data ke dalam kelompok yang koheren(Abdallah Moubayed, 2020). 
+                Metode K-Means membagi data menjadi beberapa kelompok-kelompok (cluster) dimana nanti kelompok data-data tersebut memiliki karakteristik yang sama ke dalam cluster yang sama dan kelompok data yang berbeda akan dikelompokkan pada cluster yang lain(Indah Purnama 
+                Sari, 2021).</span></div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div><!-- End K-Means -->
+
+    <!-- K Fold -->
+    <div class="col-xxl-4 col-md-12">
+      <div class="card info-card sales-card">
+
+        <div class="card-body">
+          <h5 class="card-title">Metode K-Fold <span>| Daniel Bagus Christyanto</span></h5>
+
+          <div class="d-flex">
+            <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+            <img src="assets/img/fold.png" alt="">
+            </div>
+            <div class="ps-3">
+              <h4>K-Fold</h4>
+              <span class="font-italic small pt-2 ps-1">K-Fold is one of the most commonly used cross-validation methods in statistical modeling and machine learning. It is used to test and validate model performance by dividing data into parts or "folds".</span>
+              <div><span class="text-muted small pt-2 ps-1">K-Fold adalah salah satu metode validasi silang (cross-validation) yang umum digunakan dalam pemodelan statistik dan pembelajaran mesin. Ini digunakan untuk menguji dan memvalidasi kinerja model dengan membagi data menjadi beberapa bagian atau "lipatan" (folds).</span></div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div><!-- End K-Fold -->
+
+    <!-- Reports -->
+    <div class="col-12">
+      <div class="card">
+
+        <div class="card-body">
+          <h5 class="card-title">Reports <span>/Today</span></h5>
+
+          <!-- Line Chart -->
+          <div id="reportsChart"></div>
+
+          <script>
+            document.addEventListener("DOMContentLoaded", () => {
+              new ApexCharts(document.querySelector("#reportsChart"), {
+                series: [{
+                  name: 'Sales',
+                  data: [31, 40, 28, 51, 42, 82, 56],
+                }, {
+                  name: 'Revenue',
+                  data: [11, 32, 45, 32, 34, 52, 41]
+                }, {
+                  name: 'Customers',
+                  data: [15, 11, 32, 18, 9, 24, 11]
+                }],
+                chart: {
+                  height: 350,
+                  type: 'area',
+                  toolbar: {
+                    show: false
+                  },
+                },
+                markers: {
+                  size: 4
+                },
+                colors: ['#4154f1', '#2eca6a', '#ff771d'],
+                fill: {
+                  type: "gradient",
+                  gradient: {
+                    shadeIntensity: 1,
+                    opacityFrom: 0.3,
+                    opacityTo: 0.4,
+                    stops: [0, 90, 100]
+                  }
+                },
+                dataLabels: {
+                  enabled: false
+                },
+                stroke: {
+                  curve: 'smooth',
+                  width: 2
+                },
+                xaxis: {
+                  type: 'datetime',
+                  categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+                },
+                tooltip: {
+                  x: {
+                    format: 'dd/MM/yy HH:mm'
+                  },
+                }
+              }).render();
+            });
+          </script>
+          <!-- End Line Chart -->
+
+        </div>
+
+      </div>
+    </div><!-- End Reports -->
+
+  </div>
+  </div><!-- End Left side columns -->
+
+</section>
 @endsection
