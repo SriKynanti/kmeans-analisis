88381a87a4b0d6b7c519b1f946321497d7380d2e
SriKynanti <kynanti.ti@gmail.com>
2023-06-20 17:44:57 +0700

    UI hasil perhitungan

--- resources/views/klasterisasi/hasilperhitungan.blade.php
+++ resources/views/klasterisasi/hasilperhitungan.blade.php
@@ -7,56 +7,25 @@
 
           <div class="card">
             <div class="card-body">
-              <h5 class="card-title">Hasil Klusterisasi</h5>
-              <p>Add lightweight datatables to your project with using the <a href="https://github.com/fiduswriter/Simple-DataTables" target="_blank">Simple DataTables</a> library. Just add <code>.datatable</code> class name to any table you wish to conver to a datatable</p>
+              <h4 class="card-title">Hasil Klusterisasi</h4>
 
               <!-- Table with stripped rows -->
+              <h class="card-title">ITERASI 1</h5>
               <table class="table datatable">
                 <thead>
                   <tr>
                     <th scope="col">#</th>
+                    <th scope="col">Nama</th>
                     <th scope="col">Nim</th>
                     <th scope="col">Kelas</th>
-                    <th scope="col">Nama</th>
+                    <th scope="col">Centroid 1</th>
+                    <th scope="col">Centroid 2</th>
+                    <th scope="col">Centroid 3</th>
                     <th scope="col">Kluster</th>
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Brandon Jacob</td>
-                    <td>Designer</td>
-                    <td>28</td>
-                    <td>2016-05-25</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Bridie Kessler</td>
-                    <td>Developer</td>
-                    <td>35</td>
-                    <td>2014-12-05</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Ashleigh Langosh</td>
-                    <td>Finance</td>
-                    <td>45</td>
-                    <td>2011-08-12</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">4</th>
-                    <td>Angus Grady</td>
-                    <td>HR</td>
-                    <td>34</td>
-                    <td>2012-06-11</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">5</th>
-                    <td>Raheem Lehner</td>
-                    <td>Dynamic Division Officer</td>
-                    <td>47</td>
-                    <td>2011-04-19</td>
-                  </tr>
+                  
                 </tbody>
               </table>
               <!-- End Table with stripped rows -->
